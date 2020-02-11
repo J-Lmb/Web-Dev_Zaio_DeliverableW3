72b1bde8ba4b19201de8c1c4909266a7de438420
J-Lmb <joseph.lmb.project@hotmail.com>
2020-02-11 21:24:14 +0200

    heroku hosting2

--- app.js
+++ app.js
@@ -13,5 +13,6 @@ app.use(express.static('./public'));
 todoController(app);
 
 //listen to port
-app.listen(process.env.PORT || 3000, function());
-console.log('Listening on port 3000');
+app.listen(process.env.PORT || 3000, function(){
+    console.log('Listening on port 3000');
+});
