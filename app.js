2252e290a30543ba87f628ec5c07fc901dfc2e4a
J-Lmb <joseph.lmb.project@hotmail.com>
2020-02-11 20:38:47 +0200

    Deploy for heroku

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
 var express = require('express');
-
+var todoController = require('./controllers/todoController');
 var app = express();
 
-
-var todoController = require('./controllers/todoController');
-
-//set up template engine
+//setup template engine
 app.set('view engine', 'ejs');
 
 //static files
-app.use(express.static('./Todo_App/public')); // to get the css file, you would now only need ./assets/styles.css, and not the public also
+app.use(express.static('./public'));
 
-//fire controllers
+
+//fire the controller
 todoController(app);
 
 //listen to port
-app.listen(3000);
-console.log('You are listening to port 3000');
+app.listen(process.env.PORT || 3000, function());
+console.log('Listening on port 3000');
--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,59 +1,54 @@
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
-//Connect to DB
+//Connect to the database
 mongoose.connect('mongodb+srv://dbZaio:Zaio2020Launchlab@cluster0-q9lzt.mongodb.net/test?retryWrites=true&w=majority');
 
-//Create schema
+
+//Create a schema
 var todoSchema = new mongoose.Schema({
-    item:String
+   item:String 
 });
 
-//Create model based on the schema
+//Create a todo model
 var Todo = mongoose.model('Todo', todoSchema);
-
-//Create an item and save it in DB
-/*var itemOne = Todo({item: 'update LinkedIn'}).save(function(err){ 
-    if (err) throw err;
-    console.log('item saved.');
+/*var itemOne = Todo({item: 'get ready for backend'}).save(function(err){
+    if(err) throw err;                                 console.log('item saved');  
 });*/
 
-//var data = [{item: 'Complete Zaio Deliv 3'}, {item: 'Watch all net ninja videos'}, {item: 'Practise NodeJS'}];
-
+/*var data = [{item:'update LinkedIn'},{item:'post your code'}, {item:'send report'}];*/
 var urlEncodedParser = bodyParser.urlencoded({extended: false});
 
 module.exports = function(app) {
-
-    //Get data from mongodb and pass to view
+    
     app.get('/todo', function(req, res){
         //get data from mongodb and pass it to the view
         Todo.find({}, function(err, data){
             if (err) throw err;
-            res.render('todo', {todos: data});
+            res.render('todo', {todos:data});
         });
-            
+        
     });
 
-    app.post('/todo', urlEncodedParser, function(req, res){
+    app.post('/todo', urlEncodedParser, function(req, res) {
         //get data from the view and add it to mongodb
-        var newTodo = Todo(req.body).save(function(err, data){
+        var newTodo = Todo(req.body).save(function(err,data){
             if (err) throw err;
             res.json(data);
-        })
+        });
+        //data.push(req.body);
         
-     });
+    });
 
     app.delete('/todo/:item', function(req, res){
-        //delete the requested item from mongdb
-        Todo.find({item:req.params.item.replace(/\-/g, " ")}).remove(function(err, data){
-            if (err) throw err;
-            res.json(data);
+        //delete the requested item from mongodb
+        Todo.find({item:req.params.item.replace(/\-/g, " ")}).remove(function(err,data){
+           if (err) throw err;
+           res.json(data);
         });
-        //delete todo list item
-        /*data = data.filter(function(todo)
-        {
-                return todo.item.replace(/ /g, '-') !== req.params/item;
-        });
-        res.json(data)*/
+        /*data = data.filter(function(todo){
+            return todo.item.replace(/ /g, '-') !== req.params.item;});*/
+        
+        
     });
-}
+};
